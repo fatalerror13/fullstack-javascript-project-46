909b8f39da0a2fe8be2b58e30ffc3692d731d293
Alexey Ivanov <4985900@gmail.com>
2024-01-19 19:23:06 +0300

    add commander

--- src/genDiff.ts
+++ src/genDiff.ts
@@ -1,7 +1,18 @@
 #!/usr/bin/env node
 
-const genDiff = (): void => {
-	console.log('published');
+import { Command } from 'commander';
+
+const program = new Command();
+
+const genDiff = () => {
+	program
+		.name('gendiff')
+		.description('Compares two configuration files and shows a difference.')
+		.version('0.0.1');
+
+	program.parse();
 };
 
 genDiff();
+
+export default genDiff;
